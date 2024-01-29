33f1a98a44718ccb9071b4d96fd3df3f26612e39
HanselRm <20220897@itla.edu.do>
2024-01-29 18:35:16 -0400

    init

---
+++ AppStreaming/Controllers/App.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppStreaming.Controllers
+{
+    public class App : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
