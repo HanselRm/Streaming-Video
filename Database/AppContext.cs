eabd27eb49bc6eb020073da84b59d18ca97ad9f4
HanselRm <20220897@itla.edu.do>
2024-01-29 22:02:54 -0400

    Models

---
+++ Database/AppContext.cs
@@ -0,0 +1,32 @@
+﻿using Database.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class AppContext: DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Imagen> Imagenes { get; set; }
+        public DbSet<Serie> Series { get; set; }
+        public DbSet<Productora> Productoras { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            #region tablas
+            modelBuilder.Entity<Genero>().ToTable("Productos");
+            modelBuilder.Entity<Imagen>().ToTable("Imagenes");
+            modelBuilder.Entity<Serie>().ToTable("Series");
+            modelBuilder.Entity<Productora>().ToTable("Productoras");
+            #endregion
+        }
+    }
+}
---
+++ Database/Models/Serie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Serie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Enlace { get; set; }
+        public int IdProductora { get; set; }
+        public int IdGeneroPrimario { get; set; }
+        public int? IdGeneroSecundario { get; set; }
+        public int IdImagen { get; set; }
+
+        public Productora productora { get; set; }
+        public Genero GeneroPrimario { get; set; }
+        public Genero? GeneroSecundario { get; set; }
+        public Imagen Imagen { get; set; }
+
+
+    }
+}
