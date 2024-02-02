826b1de78e3ac956370fa8875a74d81265a28b34
HanselRm <20220897@itla.edu.do>
2024-02-02 08:58:46 -0400

    productoraView-Index

---
+++ AppStreaming/Controllers/ProductoraController.cs
@@ -0,0 +1,20 @@
+﻿using LogicApplication.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppStreaming.Controllers
+{
+    public class ProductoraController : Controller
+    {
+        public readonly ProductoraService productoraService;
+
+        public ProductoraController(Database.AppContext dbContext)
+        {
+            productoraService = new(dbContext);
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await productoraService.GetAllViewModel());
+        }
+    }
+}
